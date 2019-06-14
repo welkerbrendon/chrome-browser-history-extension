3e65ec9364b84a86ae542e41bc5fb6ffa2a5c430
Brendon Welker <wel15018@byui.edu>
2019-06-13 22:35:47 -0600

    works with website

--- background.js
+++ background.js
@@ -1,26 +1,76 @@
-var tab;
+// chrome.storage.local.get(['token'], function (result) {
+//     if (!result.token) {
+//         chrome.identity.launchWebAuthFlow(
+//             {'url': 'http://localhost:8000/accounts/extension-authentication/', 'interactive': true},
+//             function(redirect_url) { 
+//                 const token = redirect_url.split('=')[1]
+//                 chrome.storage.local.set({'token': token}, function() {
+//                     console.log(`Token is set to: ${token} and saved to chrome storage.`);
+//                 }); 
+//             });
+//     }
+//     else {
+//         console.log(`Token already in storage: ${result.token}`);
+//     }
+// });
+
+chrome.identity.launchWebAuthFlow(
+    {'url': 'http://localhost:8000/accounts/extension-authentication/', 'interactive': true},
+    function(redirect_url) { 
+        const token = redirect_url.split('=')[1]
+        chrome.storage.local.set({'token': token}, function() {
+            console.log(`Token is set to: ${token} and saved to chrome storage.`);
+        }); 
+    });
+
+
+var tab = null;
 var startTime;
-var currentWindowId;
 
 function handleNewTab(newTab) {
     canonicalizeUrl(newTab);
     if (tab == null) {
         tab = newTab;
         tab.urlList = [tab.url];
-
+        sendableStartTime = new Date().toJSON().substring(10,19).replace('T','');
         startTime = new Date().getTime();
-        currentWindowId = tab.windowId;
     }
     else if (tab.canonicalizedUrl != newTab.canonicalizedUrl) {
         var endTime = new Date().getTime();
         console.log(`visted ${tab.canonicalizedUrl} for ${endTime - startTime} milliseconds.`);
         console.log(`visted these parts of the website: ${tab.urlList.toString()}`);
+        chrome.storage.local.get(['token'], function(result) {
+            console.log(`Will send following informatoin to website: 
+                    site url: ${tab.canonicalizedUrl}, start time: ${sendableStartTime}, 
+                    extensions list: ${tab.urlList.toString()}, and user token: ${result.token}`);
+            data = {
+                'token': result.token,
+                'url': tab.canonicalizedUrl,
+                'start_time': sendableStartTime
+            };
+            console.log(`Data: ${JSON.stringify(data)}`);
+            fetch("http://localhost:8000/main/activities/site/", {
+                method: 'POST',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(function(res){
+                console.log(`Status: ${res.statusText}`);
 
-        tab = newTab;
-        tab.urlList = [tab.url];
+                tab = newTab;
+                tab.urlList = [tab.url];
+                startTime = endTime;
+            })
+            .catch(function(error) {
+                console.log(`Error: ${error}`)
 
-        startTime = endTime;
-        currentWindowId = tab.windowId;
+                tab = newTab;
+                tab.urlList = [tab.url];
+                startTime = endTime;
+            });
+        });
     }
     else if (tab.url != newTab.url) {
         if (!tab.urlList.includes(newTab.url)) {
@@ -53,14 +103,14 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
     });
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    handleNewTab(tab);
-})
+/*chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, newTab) {
+    handleNewTab(newTab);
+})*/
 
-chrome.windows.onFocusChanged.addListener(function (windowId) {
+/*chrome.windows.onFocusChanged.addListener(function (windowId) {
     chrome.tabs.query({active: true, windowId: windowId}, function(results) {
         if (results.length == 1) {
             handleNewTab(results[0]);
         }
     });
-})
+})*/
